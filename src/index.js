f7269a995ede27bd06a3b5369c398bf96b3b5bde
zhuws <zhuws@authine.com>
2019-03-04 15:02:44 +0800

    output management && development --DEMO

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello','webpack','pwa'],' ');
+  let btn = document.createElement('button');
+  element.innerHTML = ['Hello','webpack','i am','pwa','\n'].join(' ');
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
