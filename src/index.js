5102d6624f9649e30620d3b1113cbd5e9c235f2b
zhuws <zhuws@authine.com>
2019-03-04 15:10:52 +0800

    hot module replacement with node.js api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import printMe from './print.js';
 function component() {
   let element = document.createElement('div');
   let btn = document.createElement('button');
-  element.innerHTML = ['Hello','webpack','i am','pwa','\n'].join(' ');
-  btn.innerHTML = 'click me and check the console';
+  element.innerHTML = ['Hello','webpack','with','pwa','\n'].join(' ');
+  btn.innerHTML = 'click here';
   btn.onclick = printMe;
   element.appendChild(btn);
   return element;
